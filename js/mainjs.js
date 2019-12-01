52acd6dae2f76697a9641a71ceb6e5347124727d
Khaled El-Tarabeely <khaled.eltarabeely@amiral.com>
2019-12-01 17:33:45 +0200

    change indicator to native javascript

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,158 +1,225 @@
-(function($) {
-  "use strict";
-  $.fn.sliderResponsive = function(settings) {
+// (function($) {
+//   "use strict";
+//   $.fn.sliderResponsive = function(settings) {
 
-    var set = $.extend( 
-      {
-        slidePause: 4000,
-        fadeSpeed: 800,
-        autoPlay: "on",
-        showArrows: "off", 
-        hideDots: "off", 
-        hoverZoom: "on",
-        titleBarTop: "off"
-      },
-      settings
-    ); 
+//     var set = $.extend( 
+//       {
+//         slidePause: 4000,
+//         fadeSpeed: 800,
+//         autoPlay: "on",
+//         showArrows: "off", 
+//         hideDots: "off", 
+//         hoverZoom: "on",
+//         titleBarTop: "off"
+//       },
+//       settings
+//     ); 
 
-    var $slider = $(this);
-    var size = $slider.find("> div").length; //number of slides
+//     var $slider = $(this);
+//     var size = $slider.find("> div").length; //number of slides
+//     var position = 0; // current position of carousal
+//     var sliderIntervalID; // used to clear autoplay
+
+//     // Add a Dot for each slide
+//     $slider.append("<ul></ul>");
+//     $slider.find("> div").each(function(){
+//       $slider.find("> ul").append('<li></li>');
+//     });
+
+//     // Put .show on the first Slide
+//     $slider.find("div:first-of-type").addClass("show");
+
+//     // Put .showLi on the first dot
+//     $slider.find("li:first-of-type").addClass("showli")
+
+//      //fadeout all items except .show
+//     $slider.find("> div").not(".show").fadeOut();
+
+//     // If Autoplay is set to 'on' than start it
+//     if (set.autoPlay === "on") {
+//         startSlider(); 
+//     } 
+
+//     // If showarrows is set to 'on' then don't hide them
+//     if (set.showArrows === "on") {
+//         $slider.addClass('showArrows'); 
+//     }
+
+//     // If hideDots is set to 'on' then hide them
+//     if (set.hideDots === "on") {
+//         $slider.addClass('hideDots'); 
+//     }
+
+//     // If hoverZoom is set to 'off' then stop it
+//     if (set.hoverZoom === "off") {
+//         $slider.addClass('hoverZoomOff'); 
+//     }
+
+//     // If titleBarTop is set to 'on' then move it up
+//     if (set.titleBarTop === "on") {
+//         $slider.addClass('titleBarTop'); 
+//     }
+
+//     // function to start auto play
+//     function startSlider() {
+//       sliderIntervalID = setInterval(function() {
+//         nextSlide();
+//       }, set.slidePause);
+//     }
+
+//     // on mouseover stop the autoplay
+//     $slider.mouseover(function() {
+//       if (set.autoPlay === "on") {
+//         clearInterval(sliderIntervalID);
+//       }
+//     });
+
+//     // on mouseout starts the autoplay
+//     $slider.mouseout(function() {
+//       if (set.autoPlay === "on") {
+//         startSlider();
+//       }
+//     });
+
+//     //on right arrow click
+//     $slider.find("> .right").click(nextSlide)
+
+//     //on left arrow click
+//     $slider.find("> .left").click(prevSlide);
+
+//     // Go to next slide
+//     function nextSlide() {
+//       position = $slider.find(".show").index() + 1;
+//       if (position > size - 1) position = 0;
+//       changeCarousel(position);
+//     }
+
+//     // Go to previous slide
+//     function prevSlide() {
+//       position = $slider.find(".show").index() - 1;
+//       if (position < 0) position = size - 1;
+//       changeCarousel(position);
+//     }
+
+//     //when user clicks slider button
+//     $slider.find(" > ul > li").click(function() {
+//       position = $(this).index();
+//       changeCarousel($(this).index());
+//     });
+
+//     //this changes the image and button selection
+//     function changeCarousel() {
+//       $slider.find(".show").removeClass("show").fadeOut();
+//       $slider
+//         .find("> div")
+//         .eq(position)
+//         .fadeIn(set.fadeSpeed)
+//         .addClass("show");
+//       // The Dots
+//       $slider.find("> ul").find(".showli").removeClass("showli");
+//       $slider.find("> ul > li").eq(position).addClass("showli");
+//     }
+
+//     return $slider;
+//   };
+// })(jQuery);
+
+
+// -------------------------------Draw Indicator slider card------------------------------------------
+function addIndicator() {
+    var sliderCard=document.getElementById("slider1");
+    var size = sliderCard.getElementsByTagName('div').length; //number of slides
+    console.log(size);
     var position = 0; // current position of carousal
-    var sliderIntervalID; // used to clear autoplay
 
-    // Add a Dot for each slide
-    $slider.append("<ul></ul>");
-    $slider.find("> div").each(function(){
-      $slider.find("> ul").append('<li></li>');
-    });
+    var ol = document.createElement('ol');
+    ol.setAttribute('class', 'carousel-indicators');
+    ol.setAttribute('id', 'list2');
+    
 
-    // Put .show on the first Slide
-    $slider.find("div:first-of-type").addClass("show");
-
-    // Put .showLi on the first dot
-    $slider.find("li:first-of-type").addClass("showli")
-
-     //fadeout all items except .show
-    $slider.find("> div").not(".show").fadeOut();
-
-    // If Autoplay is set to 'on' than start it
-    if (set.autoPlay === "on") {
-        startSlider(); 
+    document.getElementById("slider1").appendChild(ol);//append ol inside slider
+    var i=0;
+    for(i=0;i<size;i++){
+      var li = document.createElement('li');
+      document.createElement('li').setAttribute('data-slide-to', i);
+      ol.appendChild(li);
     } 
+   
 
-    // If showarrows is set to 'on' then don't hide them
-    if (set.showArrows === "on") {
-        $slider.addClass('showArrows'); 
-    }
-
-    // If hideDots is set to 'on' then hide them
-    if (set.hideDots === "on") {
-        $slider.addClass('hideDots'); 
-    }
-
-    // If hoverZoom is set to 'off' then stop it
-    if (set.hoverZoom === "off") {
-        $slider.addClass('hoverZoomOff'); 
-    }
-
-    // If titleBarTop is set to 'on' then move it up
-    if (set.titleBarTop === "on") {
-        $slider.addClass('titleBarTop'); 
-    }
-
-    // function to start auto play
-    function startSlider() {
-      sliderIntervalID = setInterval(function() {
-        nextSlide();
-      }, set.slidePause);
-    }
-
-    // on mouseover stop the autoplay
-    $slider.mouseover(function() {
-      if (set.autoPlay === "on") {
-        clearInterval(sliderIntervalID);
-      }
-    });
-
-    // on mouseout starts the autoplay
-    $slider.mouseout(function() {
-      if (set.autoPlay === "on") {
-        startSlider();
-      }
-    });
+    // Put active on the first dot
+    var liactive =sliderCard.getElementsByTagName('li')[0];
+    liactive.setAttribute("class","active");
 
     //on right arrow click
-    $slider.find("> .right").click(nextSlide)
+    // Go to next slide
+    document.getElementById("rightArrow").onclick = function(e){
+      //your handler here..
+      console.log("clickRight");
+      var domElements =document.getElementById("list2").childNodes;
+      for(var i=0;i<domElements.length;i++){
+        if(domElements[i].className==="active"){
+           // index i 
+           console.log(i);
+           position = i + 1;
+        }
+     }
+     
+      if (position > size - 1) position = 0;
+       changeCarousel(position);
+  }
 
     //on left arrow click
-    $slider.find("> .left").click(prevSlide);
-
-    // Go to next slide
-    function nextSlide() {
-      position = $slider.find(".show").index() + 1;
-      if (position > size - 1) position = 0;
-      changeCarousel(position);
-    }
-
-    // Go to previous slide
-    function prevSlide() {
-      position = $slider.find(".show").index() - 1;
+     // Go to previous slide
+    document.getElementById("leftArrow").onclick = function(e){
+      //your handler here..
+      console.log("clickLeft")
+      var domElements =document.getElementById("list2").childNodes;
+      for(var i=0;i<domElements.length;i++){
+        if(domElements[i].className==="active"){
+           // index i 
+           console.log(i);
+           position = i - 1;
+        }
+     }
+      console.log(position);
       if (position < 0) position = size - 1;
-      changeCarousel(position);
-    }
-
-    //when user clicks slider button
-    $slider.find(" > ul > li").click(function() {
-      position = $(this).index();
-      changeCarousel($(this).index());
-    });
+       changeCarousel(position);
+  }
 
     //this changes the image and button selection
     function changeCarousel() {
-      $slider.find(".show").removeClass("show").fadeOut();
-      $slider
-        .find("> div")
+      sliderCard.getElementsByClassName("active").remove("active").fadeOut();
+      sliderCard
+        .getElementsByTagName('div')
         .eq(position)
         .fadeIn(set.fadeSpeed)
-        .addClass("show");
+        .addClass("active");
       // The Dots
-      $slider.find("> ul").find(".showli").removeClass("showli");
-      $slider.find("> ul > li").eq(position).addClass("showli");
+      sliderCard.getElementsByTagName('ul').getElementsByClassName("active").remove("active");
+      sliderCard.getElementsByTagName("> ul > li").eq(position).setAttribute("class","active");
     }
+    
+}
 
-    return $slider;
-  };
-})(jQuery);
 
 
 //-------------------------------Native JavaScript On DocumentReady Function--------------------------------
 document.addEventListener("DOMContentLoaded", function (event) {
   //draw Top nav items from json
   init();
-  //slider
-  $("#slider1").sliderResponsive({
-    // Using default everything
-       slidePause: 5000,
-       fadeSpeed: 800,
-       autoPlay: "on",
-       showArrows: "off", 
-       hideDots: "off", 
-       hoverZoom: "on", 
-      // titleBarTop: "off"
-    });
+  addIndicator();
+  // //slider
+  // $("#slider1").sliderResponsive({
+  //   // Using default everything
+  //      slidePause: 5000,
+  //      fadeSpeed: 800,
+  //      autoPlay: "on",
+  //      showArrows: "off", 
+  //      hideDots: "off", 
+  //      hoverZoom: "on", 
+  //     // titleBarTop: "off"
+  //   });
 });
-
-
-//--------------------------json object------------------------
-var obj = [
-  { label: "Login", link: "#" },
-  { label: "Signup", link: "#" },
-  { label: "Help", link: "#" },
-  { label: "Become a Host", link: "#" },
-  { label: "$USB", link: "#" },
-  { label: "English", link: "#" },
-  { label: "arabic", link: "#" }
-];
 
 
 
@@ -201,9 +268,17 @@ function init(){
     }
   };
   xhttp.open("GET", "http://localhost/json.php", true);
-  xhttp.send();
-  
-    
+  xhttp.send();    
 }
 
 
+// //--------------------------json object------------------------
+// var obj = [
+//   { label: "Login", link: "#" },
+//   { label: "Signup", link: "#" },
+//   { label: "Help", link: "#" },
+//   { label: "Become a Host", link: "#" },
+//   { label: "$USB", link: "#" },
+//   { label: "English", link: "#" },
+//   { label: "arabic", link: "#" }
+// ];
