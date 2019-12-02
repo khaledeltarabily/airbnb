89759be988c15beb2d6b4175ae530de6e7a18137
Khaled El-Tarabeely <khaled.eltarabeely@amiral.com>
2019-12-02 17:49:21 +0200

    slider indicator

--- js/mainjs.js
+++ js/mainjs.js
@@ -123,97 +123,152 @@
 //     return $slider;
 //   };
 // })(jQuery);
-
+var sliderIntervalID; // used to clear autoplay
 // -------------------------------Draw Indicator slider card------------------------------------------
 function addIndicator() {
-  var sliderCard = document.getElementById("slider1");
-  var size = sliderCard.getElementsByTagName("div").length; //number of slides
-  console.log(size);
-  var position = 0; // current position of carousal
+    //var sliderCard = document.getElementById("slider1");
+    var sliderCard = document.getElementsByClassName("sliderClass");
+    console.log(sliderCard.length);
+    for (j = 0; j< sliderCard.length; j++) {
 
-  var ol = document.createElement("ol");
-  ol.setAttribute("class", "carousel-indicators");
-  ol.setAttribute("id", "list2");
+      console.log(sliderCard[j]);
+      var size = sliderCard[j].getElementsByTagName("div").length; //number of slides
+      console.log(size);
+      var position = 0; // current position of carousal
+     
+    // Put .show on the first  Slide image
+    sliderCard[j].getElementsByTagName("div")[0].setAttribute("class","show");
+  
+    var ol = document.createElement("ol");
+    ol.setAttribute("class", "carousel-indicators");
+    ol.setAttribute("id", "list2");
+  
+    sliderCard[j].appendChild(ol); //append ol inside slider
+    var i = 0;
+    for (i = 0; i < size; i++) {
+      var li = document.createElement("li");
+      li.setAttribute("data-slide-to", i);
+      ol.appendChild(li);
+    }
+  
+    // Put active on the first dot
+    var liactive = sliderCard[j].getElementsByTagName("li")[0];
+    liactive.setAttribute("class", "active");
+  
+    //on right arrow click
+    // Go to next slide
+    document.getElementById("rightArrow").onclick = function(e) {
+      nextSlide(sliderCard[j],size);
+    };
+  
+    //on left arrow click
+    // Go to previous slide
+    document.getElementById("leftArrow").onclick = function(e) {
+      console.log("clickLeft");
+      var domElements = document.getElementById("list2").childNodes;
+      for (var i = 0; i < domElements.length; i++) {
+        if (domElements[i].className === "active") {
+          // index i
+          position = i - 1;
+          console.log(position);
+        }
+      }
+      console.log(position);
+      if (position < 0) position = size - 1;
+      changeCarousel(position,domElements,sliderCard[j]);
+    };
+  
+      //start auto display
+      startSlider(sliderCard[j],size) ;
+  
+      //on hover slider 
+      sliderCard[j].addEventListener("mouseover", function( event ) {
+        console.log('called on hover'); //just to make sure the function is getting called   
+        
+         clearInterval(sliderIntervalID);
+      }, false);    
+  
+      //on hover slider 
+      sliderCard[j].addEventListener("mouseout", function( event ) {
+        console.log('mouse out'); //just to make sure the function is getting called   
+       
+        startSlider(sliderCard[j],size);
+      }, false); 
+  
+     
+  
+    var domElements = document.getElementById("list2");
+    domElements.onclick = function(e) {
+      console.log(e.target.getAttribute('data-slide-to'));
+     
+     //indicator slider remove old bullot 
+      for (var i = 0; i < domElements.childNodes.length; i++) {
+        if (domElements.childNodes[i].className === "active") {
+          domElements.childNodes[i].classList.remove("active")
+        }
+      }
+    
+      //set new bullet
+      e.target.setAttribute("class","active");
+  
+    //image slider remove old img 
+    for (var i = 0; i < sliderCard[j].getElementsByTagName("div").length; i++) {
+      if (sliderCard[j].getElementsByTagName("div")[i].className === "show") {
+        sliderCard[j].getElementsByTagName("div")[i].classList.remove("show")
+      }
+    }
+  
+    //set new image
+    sliderCard[j].getElementsByTagName("div")[e.target.getAttribute('data-slide-to')].setAttribute("class","show");
+  
+    };
+  
+  
 
-  document.getElementById("slider1").appendChild(ol); //append ol inside slider
-  var i = 0;
-  for (i = 0; i < size; i++) {
-    var li = document.createElement("li");
-    document.createElement("li").setAttribute("data-slide-to", i);
-    ol.appendChild(li);
-  }
+    }
+    
+   
+}
 
-  // Put active on the first dot
-  var liactive = sliderCard.getElementsByTagName("li")[0];
-  liactive.setAttribute("class", "active");
 
-  //on right arrow click
-  // Go to next slide
-  document.getElementById("rightArrow").onclick = function(e) {
-    //your handler here..
-    console.log("clickRight");
-    var domElements = document.getElementById("list2").childNodes;
+function startSlider(sliderCard,size) {
+ // function to start auto play
+ sliderIntervalID = setInterval( function() 
+ {
+   nextSlide(sliderCard,size);
+ }, 5000);
+}
+
+function nextSlide(sliderCard,size) {
+  var domElements = document.getElementById("list2").childNodes;
     for (var i = 0; i < domElements.length; i++) {
       if (domElements[i].className === "active") {
         // index i
-        console.log(i);
         position = i + 1;
+        console.log(position);
       }
     }
 
     if (position > size - 1) position = 0;
-    changeCarousel(position);
-  };
-
-  //on left arrow click
-  // Go to previous slide
-  document.getElementById("leftArrow").onclick = function(e) {
-    //your handler here..
-    console.log("clickLeft");
-    var domElements = document.getElementById("list2").childNodes;
-    for (var i = 0; i < domElements.length; i++) {
-      if (domElements[i].className === "active") {
-        // index i
-        console.log(i);
-        position = i - 1;
-      }
-    }
-    console.log(position);
-    if (position < 0) position = size - 1;
-    changeCarousel(position);
-  };
+    changeCarousel(position,domElements,sliderCard);
 }
 
 //this changes the image and button selection
-function changeCarousel(pos) {
-  var sliderCard = document.getElementById("slider1");
-  console.log(sliderCard.getElementsByClassName("active"));
-  sliderCard.getElementsByClassName("active").remove;
-  sliderCard
-    .getElementsByTagName("div")
-    .eq(pos)
-    .fadeIn(set.fadeSpeed)
-    .setAttribute("class", "active");
+function changeCarousel(pos,domElements,sliderCard) {
+  sliderCard.getElementsByClassName("active")[0].classList.remove("active");
+  
+  //dots
+  domElements[pos].setAttribute("class","active");
+ 
+  //image slider remove old img 
+  for (var i = 0; i < sliderCard.getElementsByTagName("div").length; i++) {
+    if (sliderCard.getElementsByTagName("div")[i].className === "show") {
+      sliderCard.getElementsByTagName("div")[i].classList.remove("show")
+    }
+  }
 
-  // The Dots
-  sliderCard
-    .getElementsByTagName("ol")
-    .getElementsByClassName("active")
-    .remove("active");
-  sliderCard
-    .getElementsByTagName("> ul > li")
-    .eq(position)
-    .setAttribute("class", "active");
-
-  //$slider.find(".show").removeClass("show").fadeOut();
-  //       $slider
-  //         .find("> div")
-  //         .eq(position)
-  //         .fadeIn(set.fadeSpeed)
-  //         .addClass("show");
-  //       // The Dots
-  //       $slider.find("> ul").find(".showli").removeClass("showli");
-  //       $slider.find("> ul > li").eq(position).addClass("showli");
+  //set new image
+  sliderCard.getElementsByTagName("div")[pos].setAttribute("class","show");
 }
 
 //-------------------------------Native JavaScript On DocumentReady Function--------------------------------
@@ -221,17 +276,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   //draw Top nav items from json
   init();
   addIndicator();
-  // //slider
-  // $("#slider1").sliderResponsive({
-  //   // Using default everything
-  //      slidePause: 5000,
-  //      fadeSpeed: 800,
-  //      autoPlay: "on",
-  //      showArrows: "off",
-  //      hideDots: "off",
-  //      hoverZoom: "on",
-  //     // titleBarTop: "off"
-  //   });
+  
 });
 
 //--------------------------calling Draw Fun-------------------------
